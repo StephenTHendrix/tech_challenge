b509bd8c23b1cf721862e0b1a6090a864b2b4b7a
Stephen Hendrix <StephenHendrix@Zebs-iMac-2.local>
2021-01-24 17:45:15 -0600

    Updates

--- src/_components/index.js
+++
@@ -1,3 +0,0 @@
-export * from './loader';
-export * from './navbar';
-export * from './sortArrow';
--- src/_constants/styleContants.js
+++
@@ -1,7 +0,0 @@
-export const colors = {
-  background: '#f8f3d4',
-  brand: '#00b8a9',
-  fadedBrand: '#f1d7c0',
-  brandBorder: '#c33052',
-  table: '#f6416c',
-};
